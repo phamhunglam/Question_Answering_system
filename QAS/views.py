c08f1dd5c782c8692cd21b273d8a098672c822ec
MimoAsu <phamhunglamsp@gmail.com>
2020-10-19 18:05:20 +0700

    fix bugs lucene

--- QAS/views.py
+++ QAS/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
+from django import forms
+from QAmodel.main import system
 
-# Create your views here.
+class QuestionForm(forms.Form):
+    question = forms.CharField()
+
+questions = ""
+
+
+
 def index(request):
-    return render(request, "QAS/index.html")
+    if request.method == "POST":
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question =form.cleaned_data["question"]
+            questions = system(question)
+            print(question)
+            return render(request, 'QAS/index.html',{
+                "form":form,
+                "questions":questions
+
+            })
+        else:
+            return render(request,'QAS/index.html',{
+                "form": form,
+                "questions":questions
+            })
+    return render(request, "QAS/index.html",{
+        "form": QuestionForm(),
+
+    })
